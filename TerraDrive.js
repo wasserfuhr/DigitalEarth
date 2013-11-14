2dc188e220f133367de1cc6d15ae68432c304c8d
RainerWasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2013-11-14 19:48:58 +0100

    Update TerraDrive.js

--- TerraDrive.js
+++ TerraDrive.js
@@ -1,3 +1,4 @@
+var TerraDrive={
  "00f4cb9e52707bd1d298f95e73e1e4293f9eddf6":{
   MediaType:"mp4",
   FileSize:10710769,
@@ -22,12 +23,28 @@
   CastId:"12XH",
   ContextUrl:"http://www.screenr.com/12XH",
   SourceUrl:"http://www.screenr.com/screencast/download/12XH"},
+ "f22e94b3742d4123e4204715ba10e0c5869bd1cf":{
+  MediaType:"mp4",
+  FileSize:12313603,
+  CastId:"19S7",
+  ContextUrl:"http://www.screenr.com/19S7",
+  SourceUrl:"http://www.screenr.com/screencast/download/19S7"},
  "c1e6ed271e747d5ce3dee8d1d66e2b5ec1c5909a":{
+  FileSize:33049651,
   MediaType:"zip",
-  //FileSize: ,
   ContextUrl:"https://code.google.com/p/bigtablenomic/downloads/detail?name=BtnWikiSvnDump_r3417.zip"},
+ "c2ebbf208d64f45c3bcd96f58ffb7412d85931dc":{
+  FileSize:12105683,
+  MediaType:"zip",
+  ContextUrl:"http://www.ngdc.noaa.gov/cgi-bin/mgg/gdas_retrieve?DATA_BASE=grddas&KEY=RaWa-7733"},
+ "a6c7ea18a5b33d7f8f62ec6a4b2b2fcc4dbc3555":{
+  MediaType:"mp4",
+  FileSize:11959333,
+  CastId:"12XH",
+  ContextUrl:"http://www.screenr.com/12XH",
+  SourceUrl:"http://www.screenr.com/screencast/download/12XH"},
  "ee69dc2cf356ccd6580b6a29185d1b7b43c645a7":{
-  //https://code.google.com/p/bigtablenomic/downloads/detail?name=DoorSix.3gp&can=2&q=#makechanges
+  ContextUrl:"https://code.google.com/p/bigtablenomic/downloads/detail?name=DoorSix.3gp",
   TagCloud:"DoorSix",
   MediaType:"3gp"},
  "f22e94b3742d4123e4204715ba10e0c5869bd1cf":{
@@ -44,10 +61,15 @@
 2de1b246dd33ea8d90943c72769eaa846d1da329.jpg
 594f0d0e37db32922a853937fe61f81b4a5fc99d.png
 8d972587a19fc69e328529b283c6243ed5a9845c.mp4
-c1e6ed271e747d5ce3dee8d1d66e2b5ec1c5909a.zip
-c2ebbf208d64f45c3bcd96f58ffb7412d85931dc.zip
 
 hoffe Du vergisst Deinen OberProgrammierLehrer nicht ganz- wünsche Dir auf jeden 
 fall einen erfolgreichen EngelsTag *ZartKuss* 
 
-jetzt wieder besser - sehn wir uns denn noch davor? ;)*/
+jetzt wieder besser - sehn wir uns denn noch davor? ;)
+
+
+//"rubens" doch ein DickChenKomplex? ;)
+
+da freue ich mich, harre Ihrer rehabilitation und hoffe auf baldiges wiedersehen ;) *GeneseKuss*
+
+*/
