f7b66614cb35600b09b8ba13c4e9925885f14a85
RainerWasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2013-11-14 17:44:41 +0100

    Update TerraDrive.js

--- TerraDrive.js
+++ TerraDrive.js
@@ -1,44 +1,41 @@
-
  "c1e6ed271e747d5ce3dee8d1d66e2b5ec1c5909a":{
   MediaType:"zip",
   //FileSize: ,
   ContextUrl:"https://code.google.com/p/bigtablenomic/downloads/detail?name=BtnWikiSvnDump_r3417.zip"},
  "ee69dc2cf356ccd6580b6a29185d1b7b43c645a7":{
-
-https://code.google.com/p/bigtablenomic/downloads/detail?name=DoorSix.3gp&can=2&q=#makechanges
+  //https://code.google.com/p/bigtablenomic/downloads/detail?name=DoorSix.3gp&can=2&q=#makechanges
  "TagCloud:"DoorSix",
   MediaType:"3gp"},
-
-  "00f4cb9e52707bd1d298f95e73e1e4293f9eddf6":{
-   MediaType:"mp4",
-   FileSize:10710769,
-   CastId:"0ZD",
-   ContextUrl:"http://www.screenr.com/0ZD",
-   SourceUrl:"http://www.screenr.com/screencast/download/0ZD"},
-  "75d727ad25b04c0b5b85e703a838c3b81c7d3b86":{
-   MediaType:"mp4",
-   FileSize:11452904,
-   CastId:"0ZkH",
-   ContextUrl:"http://www.screenr.com/0ZkH",
-   SourceUrl:"http://www.screenr.com/screencast/download/0ZkH"},
-  "a6c7ea18a5b33d7f8f62ec6a4b2b2fcc4dbc3555":{
-   MediaType:"mp4",
-   FileSize:11959333,
-   CastId:"12XH",
-   ContextUrl:"http://www.screenr.com/12XH",
-   SourceUrl:"http://www.screenr.com/screencast/download/12XH"},
-  "5b305669f73e656965bd697a30cbaeff5e0d98fd":{
-   MediaType:"mp4",
-   FileSize:11385413,
-   CastId:"15GH",
-   ContextUrl:"http://www.screenr.com/15GH",
-   SourceUrl:"http://www.screenr.com/screencast/download/15GH"},
-  "f22e94b3742d4123e4204715ba10e0c5869bd1cf":{
-   MediaType:"mp4",
-   FileSize:12313603,
-   CastId:"19S7",
-   ContextUrl:"http://www.screenr.com/19S7",
-   SourceUrl:"http://www.screenr.com/screencast/download/19S7"},
+ "00f4cb9e52707bd1d298f95e73e1e4293f9eddf6":{
+  MediaType:"mp4",
+  FileSize:10710769,
+  CastId:"0ZD",
+  ContextUrl:"http://www.screenr.com/0ZD",
+  SourceUrl:"http://www.screenr.com/screencast/download/0ZD"},
+ "75d727ad25b04c0b5b85e703a838c3b81c7d3b86":{
+  MediaType:"mp4",
+  FileSize:11452904,
+  CastId:"0ZkH",
+  ContextUrl:"http://www.screenr.com/0ZkH",
+  SourceUrl:"http://www.screenr.com/screencast/download/0ZkH"},
+ "a6c7ea18a5b33d7f8f62ec6a4b2b2fcc4dbc3555":{
+  MediaType:"mp4",
+  FileSize:11959333,
+  CastId:"12XH",
+  ContextUrl:"http://www.screenr.com/12XH",
+  SourceUrl:"http://www.screenr.com/screencast/download/12XH"},
+ "5b305669f73e656965bd697a30cbaeff5e0d98fd":{
+  MediaType:"mp4",
+  FileSize:11385413,
+  CastId:"15GH",
+  ContextUrl:"http://www.screenr.com/15GH",
+  SourceUrl:"http://www.screenr.com/screencast/download/15GH"},
+ "f22e94b3742d4123e4204715ba10e0c5869bd1cf":{
+  MediaType:"mp4",
+  FileSize:12313603,
+  CastId:"19S7",
+  ContextUrl:"http://www.screenr.com/19S7",
+  SourceUrl:"http://www.screenr.com/screencast/download/19S7"},
 };
 
 /* 0724ead894cfb653ad54f971445a6ac2a9e20d89
@@ -51,5 +48,4 @@ c2ebbf208d64f45c3bcd96f58ffb7412d85931dc.zip
 hoffe Du vergisst Deinen OberProgrammierLehrer nicht ganz- wünsche Dir auf jeden 
 fall einen erfolgreichen EngelsTag *ZartKuss* 
 
-jetzt wieder besser - sehn wir uns denn noch davor? ;)
-
+jetzt wieder besser - sehn wir uns denn noch davor? ;)*/
