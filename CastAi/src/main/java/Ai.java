fcbca7a22c17fb6543daca5276b9be4e402b192b
wasserfuhr <rawa@149-190-47-212.fsit.ch>
2013-11-17 14:27:14 +0100

    +

--- CastAi/src/main/java/Ai.java
+++ CastAi/src/main/java/Ai.java
@@ -1,22 +1,17 @@
 import java.io.File;
-//import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 import org.jcodec.api.FrameGrab;
 import org.jcodec.common.model.Picture;
 
-public class S {
-  public static void main(String[] args) throws Exception {
-      File f=new File("S117.mp4");
-    //    FrameGrab grab = new FrameGrab(new File("S111.mp4"));
-
-      Picture p=FrameGrab.getNativeFrame(f,0);
-
-      System.out.println(p.getWidth());
-	//BufferedImage frame = FrameGrab.getFrame(new File("filename.mp4"), 0);
-	//ImageIO.write(frame, "png", new File("frame_150.png"));
-    	for (int i = 0; i < 10; i++) {
-	    //	    ImageIO.write(grab.getFrame(), "png",
-	    //		  new File(System.getProperty("user.home"), String.format("frame%08d.png", i)));
-	}
-    }
+public class Ai {
+ public static void main(String[] args) throws Exception {
+  File f=new File(System.getProperty("user.home"),"TerraDrive/8d972587a19fc69e328529b283c6243ed5a9845c.mp4");
+  Picture p=FrameGrab.getNativeFrame(f,0);
+  System.out.println(p.getWidth()+"x"+p.getHeight()+":");
+  for (int i=0; i<8; i++) {
+   for (int j=0; j<8; j++) {
+       System.out.print(p.getData()[i][j]+" ");
+   }
+   System.out.println("");
+  }
+ }
 }
