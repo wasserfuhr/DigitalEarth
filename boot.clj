0a02ff3faa2c72193e674f1366b7e770a157955f
RaWa <1>
2014-09-25 21:33:43 +0200

    FreeCubes: more FortSchritt

--- boot.clj
+++ boot.clj
@@ -1,34 +1,67 @@
 (fn [rq rs db]
  (let [
-   hash (fn [msg]
-   ; https://gist.github.com/kisom/1698245
-    (let [hash (java.security.MessageDigest/getInstance "SHA-256")]
-     (. hash update (.getBytes msg))
-     (.digest hash)))
+   hash
+    (fn [msg] ; https://gist.github.com/kisom/1698245
+     (let [hash (java.security.MessageDigest/getInstance "SHA-256")]
+      (. hash update (.getBytes msg))
+      (.digest hash)))
    s (.getParameter rq "script")
-   st (.prepare db "SELECT id FROM HashChain order by CreatedAt desc")
-    hashS (hash (str 1 " " 2 " " 3))
+   formatHash
+    (fn [hash]
+     (apply str
+      (map #(format "%02x" (bit-and % 0xff))
+       hash)))
+   st0 (.prepare db "SELECT id FROM HashChain order by CreatedAt desc")
+   latestHashS (do (.step st0) (.columnBlob st0 0))
+   ;latestHash (.getBytes latestHashS)
+   st (.prepare db "SELECT id,parent,createdAt FROM HashChain order by CreatedAt asc")
    iter
     (fn [a]
-     (if (.step st) 
-      (recur (cons [:tr [:td "!" (.columnString st 0)]] a))
+     (if (.step st)
+      (recur (cons [:tr [:td (.substring (formatHash (.columnBlob st 0)) 0 8) "..."]
+         [:td (if (.columnBlob st 1) (str (.substring (formatHash (.columnBlob st 1)) 0 8) "..."))]
+         [:td
+          (.format
+           (java.text.SimpleDateFormat. "yyyy-MM-dd hh:mm:ss")
+            (java.util.Date. (.columnLong st 2)))]
+         ] a))
       a))
    rows (iter "")
    ]
  (.dispose st)
  (if s
   (let [
-    hash (hash (str (.getTime (java.util.Date.)) " " 2 " " 3))
-    dig
-     (apply str
-      (map #(format "%02x" (bit-and % 0xff))
-       hash))]
-    (.exec db (str "insert into HashChain (id,script) values ('" dig "','" s "')")))
+    now (.getTime (java.util.Date.))
+    hash
+    (hash
+     (str
+      (formatHash latestHashS) " "
+      now " " 
+      "1 " ;RaWa
+      (.length s) " " s))
+    st (.prepare db (str "insert into HashChain (id,parent,isRoot,createdAt,createdBy,script) "
+     "values (?,?,0,?,?,?)"))]
+    (.bind st 1 hash)
+    (.bind st 2 latestHashS)
+    ;(.bindNull st 2)
+    (.bind st 3 now)
+    (.bind st 4 1)
+    (.step st)
+    (.dispose st)
+    (.sendRedirect rs "/FreeCubes.jsp"))
+ (if (.getParameter rq "raw") "h"
   (hiccup.core/html "<!DOCTYPE html>"
    [:html
     [:body
      [:h1 "HelloCubes!"]
+     [:small "HashBeat: " [:span {:style "font-family:monospace"} (formatHash latestHashS)]]
      [:form
       [:textarea {:name "script"}]
+      [:input {:type "checkbox" :name "isRoot"}]
       [:input {:type "submit"}]]
-     [:br] [:table rows] hashS]]))))
+     [:h2 "HashChain:"]
+     [:table {:style "font-family:monospace"}
+      [:tr
+       [:th "id"]
+       [:th "parent"]]
+      rows]]])))))
--- src/main/webapp/FreeCubes.jsp
+++ src/main/webapp/FreeCubes.jsp
@@ -1,4 +1,11 @@
-<%@page import="java.io.*" %><%@page import="com.almworks.sqlite4java.*,
+<%@page import="
+java.io.File,
+java.nio.file.Files,
+java.nio.file.Paths,
+java.io.PushbackReader,
+java.io.StringReader,
+com.almworks.sqlite4java.SQLite,
+com.almworks.sqlite4java.SQLiteConnection,
 clojure.lang.Compiler,
 clojure.lang.IFn,
 clojure.lang.LispReader,
@@ -6,37 +13,22 @@ clojure.lang.RT"%>
 <%
 SQLiteConnection db=null;
 try {
- Cookie c=new Cookie("UnCookie","1");
- c.setMaxAge(60*60*24*30); 
- response.addCookie(c);
+ //Cookie c=new Cookie("UnCookie","1");
+ //c.setMaxAge(60*60*24*30); 
+ //response.addCookie(c);
  SQLite.setLibraryPath("/home/rawa/DigitalEarth/target/lib");
  db = new SQLiteConnection(new File("/home/rawa/UrBase"));
  db.open(true);
- db.exec("PRAGMA foreign_keys = ON;");
- SQLiteStatement st = db.prepare("SELECT id FROM HashChain order by CreatedAt desc");
- st.step();
-%>HashBeat: <%=st.columnLong(0)%><br/><%
+ db.exec("PRAGMA foreign_keys = ON");
 
- st= db.prepare("SELECT * FROM HashChain order by CreatedAt desc");// where IsRoot order by CreatedAt");
- while(st.step()) {
-%>
- <%=st.columnLong(0)%>.
- <%=st.columnLong(1)%>.<br/>
-<% }
- st.dispose();
-
-
- //... eval CloJure
  //http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from-the-contents-of-a-file
  String code=new String(java.nio.file.Files.readAllBytes(
   java.nio.file.Paths.get("/home/rawa/DigitalEarth/boot.clj")));
- //"(fn [q s] \"HelloCubes\")";
  
-  RT.loadResourceScript("hiccup/core.clj");
-  PushbackReader pr = new PushbackReader(new StringReader(code));
+ //eval CloJure:
+ RT.loadResourceScript("hiccup/core.clj");
+ PushbackReader pr = new PushbackReader(new StringReader(code));
  Object rootHandlerExpr=LispReader.read( pr, true, null, false);
  IFn rootHandlerFn=(IFn) Compiler.eval( rootHandlerExpr);
-//  application.setAttribute("RootHandlerFn", rootHandlerFn);
- // memCache.put("RootHandlerFn", rootHandlerFn);
 %><%=rootHandlerFn.invoke(request,response,db)%><%
 } finally { db.dispose();}%>
