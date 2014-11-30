e83fbde6a602cbc7b019ef36275d2775a311ee2f
RaWa <1>
2014-11-30 17:28:52 +0100

    ~TimeToHilde.clj

--- src/main/webapp/TimeToHilde.clj
+++ src/main/webapp/TimeToHilde.clj
@@ -23,26 +23,41 @@ a {
     [:canvas#c {:width 320 :height 40}]
     [:script "
 var pins={
- "1111110",//0
- "0110000",
- "1101101",
+ '1111110",//0
+ '0110000",
+ '1101101",
  "1111001",
  "0110011",//4
  "1011011",
  "1011111",
  "1110000",
- "1111111";//8
- "1111011";
-};
+ "1111111",//8
+ "1111011",
+ "1110111",
+ "0011111",
+ "1001110",
+ "0111101",
+ "1001111",
+ "1000111"};
 var ctx=document.getElementById('c').getContext('2d');
 
-function digit(offset) {
+function digit(v,x) {
+  ctx.beginPath();
+  ctx.moveTo(x+80,10);
+  ctx.lineTo(x+70,70);
+  ctx.lineTo(x+70,70);
+  ctx.stroke();
+}
+
 function tick() {
  ctx.fillStyle='#0f0';
  ctx.fillRect(0,0,1024,512);
  var now=new Date();
  var t0=Math.floor(now.getTime()/1000);
-
+ for (var i=0; i<8; i++) {
+  digit(i,i);
+ }
+}
 
 setInterval(tick,1000);
 "]])))
