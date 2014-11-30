3c91195ace5b5f1cd9eace15d719e608c8784c23
RaWa <1>
2014-11-30 10:31:39 +0100

    +UnCc.clj

--- src/main/webapp/UnCc.clj
+++ src/main/webapp/UnCc.clj
@@ -1,6 +1,12 @@
 (fn [rq rs]
  (let [
+   reg #"[A-Z]+[a-z]+[A-Z]+[a-zA-Z0-9]*"
    f (java.io.File. "/home/rawa/GitHoster/GitHub/wasserfuhr/DigitalEarth/unpartei/mainSpace/")]
   (apply str 
-   (map (fn [l] (str l " "))
+   (map
+    (fn [l]
+     (let [
+       fl (re-find reg l)
+       tag (first fl)]
+      (str (if (and (not-empty fl) (== (.length fl) (.length l))) "+" "--") l " ")))
     (.list f)))))
