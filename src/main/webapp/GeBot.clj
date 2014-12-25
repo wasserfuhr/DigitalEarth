13819dbed0b2c822dae4757deb4ca66dd9d4b6a8
RaWa <1>
2014-12-26 00:36:58 +0100

    ~GeBot.clj

--- src/main/webapp/GeBot.clj
+++ src/main/webapp/GeBot.clj
@@ -23,16 +23,24 @@ a {
     [:canvas#c {:width 640 :height 480}]
     [:br]
     [:a#st {:href "http://film.sl4.eu/"}]
-    [:input {:value "+" :type "button" :onclick "rot(0,10)"}]
-    [:input {:value "-" :type "button" :onclick "rot(1,-10)"}]
+    [:input {:value "-" :type "button" :onclick "rota0(-10)"}]
+    [:input {:value "+" :type "button" :onclick "rota0(10)"}]
+    " "
+    [:input {:value "-" :type "button" :onclick "rota1(-10)"}]
+    [:input {:value "+" :type "button" :onclick "rota1(10)"}]
     [:script "
 var c=document.getElementById('c');
 var ctx=c.getContext('2d');
-var rot1=180;
+var rot0=-70;
+var rot1=-45;
 
 var start=new Date().getTime();
 
-function rot(a,b) {
+function rota0(b) {
+ rot0+=b;
+ tick();
+}
+function rota1(b) {
  rot1+=b;
  tick();
 }
@@ -42,18 +50,31 @@ var h=180*2;
 c.width=w;
 c.height=h;
 function tick() {
+ ctx.setTransform(1,0,0,1,0,0);
  var p=(new Date().getTime()-start)/1000;
  ctx.fillStyle='#111';
  ctx.fillRect(0,0,w,h);
- ctx.strokeStyle='orange';//#0f0';
+ ctx.fillStyle='orange';
+ ctx.strokeStyle='orange';
  ctx.beginPath();
- ctx.moveTo(100,100);
- ctx.lineTo(100+50*Math.sin(rot1*Math.PI/180),100+50*Math.cos(rot1*Math.PI/180));
+ ctx.translate(200,200);
+ ctx.rotate(rot0*Math.PI/180);
+ ctx.moveTo(0,-12);
+ ctx.lineTo(100,-6);
+ ctx.lineTo(100,6);
+ ctx.lineTo(0,12);//+50*Math.sin(rot1*Math.PI/180),100+50*Math.cos(rot1*Math.PI/180));
  ctx.stroke();
- ctx.rotate(-(90+rot1)*Math.PI/180);
- ctx.strokeText('PiBo',100,100);
- ctx.rotate((90+rot1)*Math.PI/180);
- ctx.setTransform(0,0,0,0,0,0);
+ ctx.textBaseline='middle';
+ ctx.font='bold 12px Courier';
+ ctx.fillText('PiBo',10,0);
+ ctx.beginPath();
+ ctx.translate(100,0);
+ ctx.rotate(rot1*Math.PI/180);
+ ctx.moveTo(0,-6);
+ ctx.lineTo(80,-4);
+ ctx.lineTo(80,4);
+ ctx.lineTo(0,6);
+ ctx.stroke();
 }
 //setInterval(tick,100);
 tick();
