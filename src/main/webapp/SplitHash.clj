b9e95de754886d0e836a36683b40047e9aa7d528
RaWa <1>
2015-02-04 07:17:38 +0100

    ~SplitHash.clj

--- src/main/webapp/SplitHash.clj
+++ src/main/webapp/SplitHash.clj
@@ -1,5 +1,7 @@
 (fn [rq rs]
  (let [
+   sl 16
+   bls (bit-shift-left 1 sl)
    h "b75876be5d725e220908d7bb6ffd267014b55c88"
    f (java.io.File. (str "/home/rawa/TerraDrive/" h ".zip"))]
   (.length f)))
