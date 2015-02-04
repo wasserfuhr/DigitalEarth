e5a7cd90481cb9d4715b5169de7808fd3c7102ed
RaWa <1>
2015-02-04 06:46:50 +0100

    ~SplitHash.clj

--- src/main/webapp/SplitHash.clj
+++ src/main/webapp/SplitHash.clj
@@ -1,4 +1,5 @@
 (fn [rq rs]
- (.length (java.io.File. "/home/rawa/GitHoster/GitHub/wasserfuhr/DigitalEarth/src/main/webapp/img/b75876be5d725e220908d7bb6ffd267014b55c88.zip")
-)
- )
+ (let [
+   h "b75876be5d725e220908d7bb6ffd267014b55c88"
+   f (java.io.File. (str "/home/rawa/TerraDrive/" h ".zip"))]
+  (.length f)))
