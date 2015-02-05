d62f2a5baa743476c4e7f98f25730f8ffd317807
RaWa <1>
2015-02-05 18:14:51 +0100

    ~SplitHash.clj

--- src/main/webapp/SplitHash.clj
+++ src/main/webapp/SplitHash.clj
@@ -2,6 +2,7 @@
  (let [
    sl 16
    bls (bit-shift-left 1 sl)
+   p (.getPath (java.nio.file.FileSystems/getDefault) "/home/rawa/TerraDrive/" "")
    h "b75876be5d725e220908d7bb6ffd267014b55c88"
    f (java.io.File. (str "/home/rawa/TerraDrive/" h ".zip"))]
   (.length f)))
