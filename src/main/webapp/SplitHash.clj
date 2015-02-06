6dc36af0a6645f0a530e46b3c20174f0c82d5b76
RaWa <1>
2015-02-06 01:40:06 +0100

    ~SplitHash.clj

--- src/main/webapp/SplitHash.clj
+++ src/main/webapp/SplitHash.clj
@@ -2,15 +2,34 @@
  (let [
    sl 16
    bls (bit-shift-left 1 sl)
+   hash
+    (fn [msg] ; https://gist.github.com/kisom/1698245
+     (let [h (java.security.MessageDigest/getInstance "SHA-256")]
+      (.update h msg)
+      (.digest h)))
    p (java.nio.file.Paths/get "/home/rawa/TerraDrive/" (into-array String []))
+   formatHash
+    (fn [h]
+     (apply str
+      (map #(format "%02x" (bit-and % 0xff)) h)))
+   ;386edd6afdeb28827661d0f5419cbbf96a9db627a6f73ee2308629053112b644
    h "b75876be5d725e220908d7bb6ffd267014b55c88"
    pl (.iterator (java.nio.file.Files/newDirectoryStream p (str h ".*")))
    first (.next pl)
    f (.toFile first)
-   l 15;(.length f)
+   l (.length f)
    l1 (bit-shift-right (+ l 1) 1)
    l2 (- l l1)
+r (java.io.RandomAccessFile. f "r")
+b1 (byte-array l1)
+b2 (byte-array l2)
 ]
-  (if (.hasNext pl)
-   "*Duplicate*!!"
-   (str l1 " " l2))))
+  (do
+   (.read r b1)
+   (.read r b2)
+   (if (.hasNext pl)
+    "*Duplicate*!!"
+    (str l1 " " l2 " " 
+     (formatHash (hash b1)) " "
+     (formatHash (hash b2))
+)))))
