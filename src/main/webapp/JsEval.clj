b06b30d65ff38cfa831b9fec26fd7e7f15229ae1
RaWa <1>
2015-08-03 01:28:54 +0200

    ~JsEval.clj

--- src/main/webapp/JsEval.clj
+++ src/main/webapp/JsEval.clj
@@ -1,6 +1,7 @@
 (fn [rq rs]
  (let [
    inSize 43
+   style "font-size:11px; font-family:monospace;margin:0;padding:0"
    v0 " 123456789abcdef"
    v1 " 123456789a"
    v (str "0123456789abcdef" v0 v1)
@@ -12,9 +13,24 @@
   (hiccup.core/html "<!DOCTYPE html>"
    [:html
     [:head
-     [:title "SuperWiki « SemperBase"]
+     [:title "WelCome « NooSphere"]
      [:meta {:name "viewport" :content "width=240"}]
      [:meta {:http-equiv "Content-type" :content "text/html; charset=utf-8"}]
+     [:script "
+var bytes='" (str v0 v0 v0)"';
+function edit(l) {
+ sp0='                       ';
+ spx=sp0+sp0+sp0+sp0;
+ var l0=document.getElementById('l0');
+ var l1=document.getElementById('l1');
+ var l2=document.getElementById('l2');
+ sS=document.getElementById('l'+l).selectionStart;
+ bytes=l0.value+l1.value+l2.value+spx;
+ l0.value=bytes.substring(0,43);
+ l1.value=bytes.substring(43,86);
+ document.getElementById('l'+l).selectionStart=sS;
+ document.getElementById('l'+l).selectionEnd=sS;
+}"]
      [:style {:type "text/css"} "
 body {
  background: #000;
@@ -25,16 +41,14 @@ a {
  color: #0f0;
 }"]]
    [:body
-    "/ - "
-    [:a#st {:href "http://time.sl4.eu/"}]
+    [:a {:href "/1"} "RaWa"] "@" [:a#st {:href "http://time.sl4.eu/"} "st00000000"]
+    ": /"
     [:br]
     [:div {:style "text-align:left"}
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size inSize :value v}] [:br]
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size inSize :value v}] [:br]
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size inSize :value v}] [:br]
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size inSize :value v}] [:br]
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size inSize :value v}] [:br]
-     [:input {:style "font-size:11px; font-family:monospace;margin:0;padding:0" :size (- inSize 2) :value v}] 
+     [:input#l0 {:style style :size inSize :value v :oninput "edit(0)"}] [:br]
+     [:input#l1 {:style style :size inSize :value v :oninput "edit(1)"}] [:br]
+     [:input#l2 {:style style :size inSize :value v :oninput "edit(2)"}] [:br]
+     [:input {:style style :size (- inSize 2) :value v}] 
      [:span#ct "ff"] [:br]
 ;"//23456789abcdef 123456789abcdef
 ;
