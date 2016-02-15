d594845113b6ee3769c9f60987bb682cb042bf81
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-15 18:54:33 +0100

    ~pull.clj

--- src/main/webapp/pull.clj
+++ src/main/webapp/pull.clj
@@ -1,4 +1,5 @@
 (fn[rq rs]
- (str"var as=["(apply str
-  (map(fn[l](str "'"l"',"))
-   (drop 2 (reverse (.split(slurp"https://dresdenlabs.appspot.com/raw/17776001")"\r\n")))))"];"))
+;(str"var as=["
+ (apply str
+  ;(map(fn[l](str "'"l"',"))
+   (filter #(.startsWith % "p:")(.split(slurp"https://dresdenlabs.appspot.com/raw/22086002")"\r\n"))))
