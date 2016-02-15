d0cbc3852307db97c90693dfadced964388a7d35
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-16 00:43:30 +0100

    ~crush.clj

--- src/main/webapp/crush.clj
+++ src/main/webapp/crush.clj
@@ -1,9 +1,19 @@
 (fn[rq rs]
- (do
-  (.setCharacterEncoding rs "UTF-8")
-  (hiccup.core/html "<!DOCTYPE html>" [:html
+ (hiccup.core/html "<!DOCTYPE html>" [:html
 [:body
- [:canvas#c]
-[:table[:tr[:td[:input{:type "checkbox"}]]]]
-]
- ])))
+ [:table
+  [:tr[:td#v]]
+  [:tr[:td[:input#i3{:type "checkbox"}][:input#i2{:type "checkbox"}]]]
+  [:tr[:td[:input#i1{:type "checkbox"}][:input#i0{:type "checkbox"}]]]]
+[:script "
+function tick(){
+ v=0;
+ for(i=0;i<4;i++){
+  if(document.getElementById('i'+i).checked){
+   v+=1<<i;
+  }
+ }
+ document.getElementById('v').innerHTML=v;
+}
+setInterval(tick,1000);"
+]]]))
