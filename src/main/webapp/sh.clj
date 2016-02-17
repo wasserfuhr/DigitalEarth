d7b006c1461c4f54650cf41c60476631b6610ae7
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-17 17:48:16 +0100

    +hot.clj ~news.clj ~sh.clj ~think.clj

--- src/main/webapp/sh.clj
+++ src/main/webapp/sh.clj
@@ -1,1 +1,4 @@
-(fn[rq rs](slurp"/home/rawa/SpaceDrive/1220eefd82ec2863d0733ce9c823c8bd18bfdfd59bd6073efa9eec5abd96c330c127.html"))
+(fn[rq rs](slurp
+(str "/home/rawa/SpaceDrive/1220"
+"eefd82ec2863d0733ce9c823c8bd18bfdfd59bd6073efa9eec5abd96c330c127"
+".html")))
