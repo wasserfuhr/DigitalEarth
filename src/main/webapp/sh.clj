0c6a8fd955e6426171d8d76c846ed7870794005c
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-17 18:20:15 +0100

    ~sh.clj

--- src/main/webapp/sh.clj
+++ src/main/webapp/sh.clj
@@ -1,8 +1,7 @@
-(fn[rq rs]
- (let[s(.getParameter rq"s")]
-  (first (filter (fn[l](= s(first (.split l" "))))
-
- (clojure.string/split-lines (slurp"/home/rawa/git/AllHashes/ShortHash.txt"))))))
+(fn[rq rs](let[s(.getParameter rq"s")]
+  (clojure.string/replace(first
+    (filter(fn[l](= s(first(.split l" "))))
+     (clojure.string/split-lines(slurp"/home/rawa/git/AllHashes/ShortHash.txt"))))" """)))
 ;(slurp
 ;(str "/home/rawa/SpaceDrive/1220"
 ;"eefd82ec2863d0733ce9c823c8bd18bfdfd59bd6073efa9eec5abd96c330c127"
