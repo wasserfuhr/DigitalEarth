ef7bc16f5532a024795099dc0f85c5b093b84b2f
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-17 21:04:56 +0100

    ~hot.clj ~img.clj

--- src/main/webapp/hot.clj
+++ src/main/webapp/hot.clj
@@ -3,16 +3,4 @@
   [:title"BitCrush"]
   [:link{:rel"stylesheet":type"text/css":href"http://sl4.eu/css"}]
   [:style"#v{font-family:monospace;font-size:200%;text-align:center}"]]
- [:body
-  [:table
-   [:tr[:td#v]]
-   [:tr[:td[:input#i3{:type"checkbox"}][:input#i2{:type"checkbox"}]]]
-   [:tr[:td[:input#i1{:type"checkbox"}][:input#i0{:type"checkbox"}]]]]
-   [:script"
-function tick(){
- v=0;
- for(i=0;i<4;i++){
-  if(document.getElementById('i'+i).checked){
-   v+=1<<i;}}
- document.getElementById('v').innerHTML='0123456789abcdef'.substring(v,v+1);}
-setInterval(tick,1000);"]]]))
+ [:body[:img {:src "/RootHandler.jsp?p=img&h=a44bc88ec1e694c49ed3bf0a330bdb1f451eaa930f22dbeae63b6565d12d31e0"}]]]))
--- src/main/webapp/img.clj
+++ src/main/webapp/img.clj
@@ -1,4 +1,1 @@
-(fn[rq rs]
- (let[sh(fn[h](slurp(str"http://127.0.0.1:8080/RootHandler.jsp?p=sh&s="h)))
-   h(fn[x](slurp(str"/home/rawa/SpaceDrive/1220"x".html")))]
-  (str"At AlphaLabs we "(subs(h(sh"ee"))0x35a 0x406)":")))
+(fn[rq rs](slurp(str"/home/rawa/SpaceDrive/1220"(.getParameter rq"h")".jpg")))
