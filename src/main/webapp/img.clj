37743df5507feba662ea0dcbf62060b22d1d1e44
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-18 12:15:00 +0100

    ~img.clj

--- src/main/webapp/img.clj
+++ src/main/webapp/img.clj
@@ -1,14 +1,3 @@
 (fn[rq rs](do (.setContentType rs"image/jpg")
-;java.util.ByteArrayOutputStream (.getBytes
-
- (clojure.java.io/copy (clojure.java.io/file (str"/home/rawa/SpaceDrive/1220"(.getParameter rq"h")".jpg"))
-  (.getOutputStream rs)
-)))
-
-
-
-;response.setContentLength(baos.size());
-;OutputStream os = response
-
-; baos.writeTo(os);
- 
+ (clojure.java.io/copy(java.io.File. (str"/home/rawa/SpaceDrive/1220"(.getParameter rq"h")".jpg"))
+  (.getOutputStream rs))))
