28ab0b9ffd0bf243c78bdc23ed38409bfac685ed
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-19 13:05:15 +0100

    ~init.clj ~think.clj

--- src/main/webapp/init.clj
+++ src/main/webapp/init.clj
@@ -1,2 +1,3 @@
 {:sh(fn[h](slurp(str"http://127.0.0.1:8080/RootHandler.jsp?p=sh&s="h)))
- :h(fn[x](slurp(str"/home/rawa/SpaceDrive/1220"x(if(.startsWith x "ee")".html"".txt"))))}
+ :h(fn[x](slurp(str"/home/rawa/SpaceDrive/1220"x(if(.startsWith x "ee")".html"".txt"))))
+ :ht(fn[x](hiccup.core/html"<!DOCTYPE html>"[:html x]))}
--- src/main/webapp/think.clj
+++ src/main/webapp/think.clj
@@ -1,2 +1,3 @@
 (fn[rq rs](let[c(.getAttribute rq"c")]
- (str"At AlphaLabs we "(subs((:h c)((:sh c)"ee"))0x35a 0x406)":")))
+;/AiM<a href="/wiki/AiM"
+ ((:ht c)(str"At AlphaLabs we "(subs((:h c)((:sh c)"ee"))0x35a 0x406)":"))))
