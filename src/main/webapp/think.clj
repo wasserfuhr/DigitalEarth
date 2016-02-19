df6ae82419cd4dec342d39f2383482e8c37b858b
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-19 01:48:50 +0100

    ~think.clj

--- src/main/webapp/think.clj
+++ src/main/webapp/think.clj
@@ -1,4 +1,2 @@
-(fn[rq rs]
- (let[sh(fn[h](slurp(str"http://127.0.0.1:8080/RootHandler.jsp?p=sh&s="h)))
-   h(fn[x](slurp(str"/home/rawa/SpaceDrive/1220"x".html")))]
-  (str"At AlphaLabs we "(subs(h(sh"ee"))0x35a 0x406)":")))
+(fn[rq rs](let[c(.getAttribute rq"c")]
+ (str"At AlphaLabs we "(subs((:h c)((:sh c)"ee"))0x35a 0x406)":")))
