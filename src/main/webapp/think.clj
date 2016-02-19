2f684386134b374e4cacb97ad3e2b1b94bb55810
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-19 13:16:02 +0100

    ~think.clj

--- src/main/webapp/think.clj
+++ src/main/webapp/think.clj
@@ -1,3 +1,5 @@
 (fn[rq rs](let[c(.getAttribute rq"c")]
-;/AiM<a href="/wiki/AiM"
- ((:ht c)(str"At AlphaLabs we "(subs((:h c)((:sh c)"ee"))0x35a 0x406)":"))))
+ ((:ht c)[:body
+  (.replace
+   (str"we "(subs((:h c)((:sh c)"ee"))0x35a 0x406)) ";machine" ";<a href='/wiki/AiMachine'>machine</a>")
+])))
