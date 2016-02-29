cc50a73fb95d7be8a1e577c8db188e7e67b1f1c9
RainerWasserfuhr <wasserfuhr@yahoo.com>
2016-03-01 00:13:48 +0100

    Update NoSh.clj

--- NoSh.clj
+++ NoSh.clj
@@ -25,6 +25,8 @@
       size (.readInt rf)
       sx (byte-array size)]
      (.read rf sx)
+
      ((eval (read-string (String. sx))) rq rs rf)))
     ;start reached!: (if (.equals "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b" (formatHash x))
    "MagicWord mismatch!"))))))
+i
