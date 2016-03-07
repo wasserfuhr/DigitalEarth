4e7dedb1eef55fe3c25af7d47b77ffb1248eb67e
RainerWasserfuhr <wasserfuhr@yahoo.com>
2016-03-07 14:56:06 +0100

    Update boot.clj

--- SemperBase/boot.clj
+++ SemperBase/boot.clj
@@ -1,14 +1,14 @@
-(fn [request response] 
- (let [
-   b "/home/rawa/DigitalEarth/SemperBase/base"
+(fn[rq rs]
+ (let[
+   b"/home/rawa/DigitalEarth/SemperBase/base"
    //String b="/home/rawa/DigitalEarth/SemperBase/base";
-   f (java.io.File. b)
+   f(java.io.File. b)
    //File f=new java.io.File(b);
-   db (com.almworks.sqlite4java.SQLiteConnection. f)
-   st (.prepare db "select id, from l order by id desc limit 32")
+   db(com.almworks.sqlite4java.SQLiteConnection. f)
+   st(.prepare db "select id, from l order by id desc limit 32")
    lines
-    (fn [st]
-     (if (.step st)
+    (fn[st]
+     (if(.step st)
       (str "<td>"st.columnInt(1)
        (lines st))]
   (str "<form><input type='submit'/><textarea name=''></textarea></form>
