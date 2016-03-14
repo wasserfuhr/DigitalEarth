9905e64cec1e82ab94b514ac2dc3550407ea7466
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-03-14 18:39:23 +0100

    ~h.clj

--- src/main/webapp/h.clj
+++ src/main/webapp/h.clj
@@ -1,3 +1,3 @@
 (fn[rq rs](do(let[h(.getParameter rq"h")](.setContentType rs"image/jpg")
- (clojure.java.io/copy(java.io.File.(str"/home/rawa/SpaceDrive/1220"h(if (.startsWith h"a3")".mp3"".3gp")))
+ (clojure.java.io/copy(java.io.File.(str"/home/rawa/SpaceDrive/1220"h(if (.startsWith h"b3")".3gp"".mp3")))
   (.getOutputStream rs)))))
