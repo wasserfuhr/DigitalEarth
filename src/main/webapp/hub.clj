a3e05b84b9a8157d64b93eff90d2311dc1e6d12b
wasserfuhr <rainerwasserfuhr+GitHub@gmail.com>
2016-04-04 13:01:24 +0200

    ~hub.clj

--- src/main/webapp/hub.clj
+++ src/main/webapp/hub.clj
@@ -1,2 +1,1 @@
-(fn[rq rs](if(.startsWith(.getRemoteAddr rq)"107.178.194.")
- (clojure.java.shell/sh(.getParameter rq"c")(.getParameter rq"o"))))
+(fn[rq rs](clojure.java.shell/sh"git""pull":dir"/home/rawa/git/un"))
