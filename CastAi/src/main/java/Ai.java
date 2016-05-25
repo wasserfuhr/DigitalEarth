948f3bdff024e824b852f51db6a04d8abf98bdd8
RainerWasserfuhr <wasserfuhr@yahoo.com>
2016-05-25 20:54:05 +0200

    +ad13c

--- CastAi/src/main/java/Ai.java
+++ CastAi/src/main/java/Ai.java
@@ -6,7 +6,7 @@ public class Ai{
   File f=new File(System.getProperty("user.home"),"TerraDrive/8d972587a19fc69e328529b283c6243ed5a9845c.mp4");
   Picture p=FrameGrab.getNativeFrame(f,0);
   System.out.println(p.getWidth()+"x"+p.getHeight()+":");
-  for (int i=0; i<8; i++) {
-   for (int j=0; j<8; j++) {
+  for(int i=0; i<8; i++) {
+   for(int j=0; j<8; j++) {
     System.out.print(p.getData()[i][j]+" ");}
    System.out.println("");}}}
